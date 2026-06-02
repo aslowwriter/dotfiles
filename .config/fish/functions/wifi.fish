064cf8edfb05d79542b0caaa7c111bf140fac93e
Sam Vente <savente93@proton.me>
2026-06-02 16:37:49 +0200

    fix: don't use ncli if ssid is empty in wifi

--- .config/fish/functions/wifi.fish
+++ .config/fish/functions/wifi.fish
@@ -1,4 +1,7 @@
 
 function wifi
-    nmcli d wifi connect -a "$(nmcli -f SSID d wifi list | sort | uniq | grep -v -F -e 'SSID' -e '--' | string trim | fzf --header "Please select a wifi network")"
+    set ssid (nmcli -f SSID d wifi list | sort | uniq | grep -v -F -e 'SSID' -e '--' | string trim | fzf --header "Please select a wifi network")
+    if test -n "$ssid"
+	nmcli d wifi connect -a $ssid
+    end
 end
