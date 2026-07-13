b3f4f85858ff624ecbe58145ce00030fe5e8f7b5
Sam Vente <savente93@proton.me>
2026-07-13 16:11:45 +0200

    fix: rewrite zola.nvim config

--- .config/nvim/lua/plugins/zola.lua
+++ .config/nvim/lua/plugins/zola.lua
@@ -1,45 +1,90 @@
 return {
-  'aslowwriter/zola.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    vim.keymap.set('n', '<leader>zbd', function()
-      require('zola').build { drafts = true }
-    end, { desc = 'Build version of site including drafts' })
+  dir = '~/projects/lua/zola.nvim',
+  lazy = false,
 
-    vim.keymap.set('n', '<leader>zbp', function()
-      require('zola').build {}
-    end, { desc = 'Build release version of site' })
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'saghen/blink.cmp',
+  },
 
-    vim.keymap.set('n', '<leader>zc', function()
-      require('zola').check {}
-    end, { desc = 'Check the site' })
+  opts = {},
 
-    vim.keymap.set('n', '<leader>zsp', function()
-      require('zola').serve { drafts = true }
-    end, { desc = 'Serve the release version of the site' })
+  keys = {
+    {
+      '<leader>zbd',
+      function()
+        require('zola').build { drafts = true }
+      end,
+      desc = 'Build version of site including drafts',
+    },
 
-    vim.keymap.set('n', '<leader>zsd', function()
-      require('zola').serve { drafts = true }
-    end, { desc = 'Serve the site with drafts' })
-    vim.keymap.set('n', '<leader>zns', function()
-      vim.ui.input({ prompt = 'Enter section slug: ' }, function(result)
-        require('zola').create_section {
-          slug = 'blog/' .. result,
-          draft = true,
-          open = true,
-        }
-      end)
-    end, { desc = 'Create a new blog section' })
+    {
+      '<leader>zbp',
+      function()
+        require('zola').build {}
+      end,
+      desc = 'Build release version of site',
+    },
 
-    vim.keymap.set('n', '<leader>znp', function()
-      vim.ui.input({ prompt = 'Enter page slug: ' }, function(result)
-        require('zola').create_page {
-          slug = 'blog/' .. result,
-          page_is_dir = true,
-          draft = true,
-          open = true,
-        }
-      end)
-    end, { desc = 'Create a new blog post' })
-  end,
+    {
+      '<leader>zc',
+      function()
+        require('zola').check {}
+      end,
+      desc = 'Check the site',
+    },
+
+    {
+      '<leader>zsp',
+      function()
+        require('zola').serve {}
+      end,
+      desc = 'Serve the release version of the site',
+    },
+
+    {
+      '<leader>zsd',
+      function()
+        require('zola').serve { drafts = true }
+      end,
+      desc = 'Serve the site with drafts',
+    },
+
+    {
+      '<leader>zns',
+      function()
+        vim.ui.input({ prompt = 'Enter section slug: ' }, function(result)
+          if not result then
+            return
+          end
+
+          require('zola').create_section {
+            slug = 'blog/' .. result,
+            draft = true,
+            open = true,
+          }
+        end)
+      end,
+      desc = 'Create a new blog section',
+    },
+
+    {
+      '<leader>znp',
+      function()
+        vim.ui.input({ prompt = 'Enter page slug: ' }, function(result)
+          if not result then
+            return
+          end
+
+          require('zola').create_page {
+            slug = 'blog/' .. result,
+            page_is_dir = true,
+            draft = true,
+            open = true,
+          }
+        end)
+      end,
+      desc = 'Create a new blog post',
+    },
+  },
 }
