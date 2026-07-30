0f8acad929a7bddb7bbf2682ba5db7c6bc29eacb
Sam Vente <savente93@proton.me>
2026-07-30 20:45:21 +0200

    feat(de): let's try KDE instead of sway for once

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -35,7 +35,7 @@ reflector
 pacman -Sy --needed archlinux-keyring --noconfirm
 
 #install what we'll need for system setup after reboot
-pacstrap -K /mnt amd-ucode base curl foot git linux linux-firmware mesa networkmanager openssl sddm sudo sway vim wikiman xorg-server-xwayland
+pacstrap -K /mnt amd-ucode base base-devel curl git linux linux-firmware mesa networkmanager openssl sddm sudo vim xorg-server-xwayland
 
 genfstab -U /mnt >>/mnt/etc/fstab
 
--- setup/jailed.sh
+++ setup/jailed.sh
@@ -1,10 +1,11 @@
 #setup system time
-ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+timedatectl set-timezone Europe/Amsterdam
 hwclock --systohc
 
 systemctl enable systemd-timesyncd.service
 systemctl enable sddm.service
 systemctl enable NetworkManager
+systemctl enable fstrim.timer
 
 sed 's/#en_GB.UTF-8.*/en_GB.UTF-8 UTF-8/g;s/#en_US.UTF-8.*/en_US.UTF-8 UTF-8/g' -i /etc/locale.gen
 locale-gen
@@ -20,10 +21,11 @@ timeout  1
 console-mode max
 editor   no' >/boot/loader/loader.conf
 
-echo "title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /initramfs-linux.img
-options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rw" >/boot/loader/entries/arch.conf
+echo "title Arch Linux
+linux /vmlinuz-linux
+initrd /amd-ucode.img
+initrd /initramfs-linux.img
+options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rw" > /boot/loader/entries/arch.conf
 
 useradd -m -G wheel sam
 
@@ -41,12 +43,10 @@ else
 fi
 
 passwd sam
-mkdir -p /home/sam/.config/sway
-cp /etc/sway/config /home/sam/.config/sway
-chown -R sam /home/sam/.config
+chown -R sam:sam /home/sam/.config
 
 curl https://raw.githubusercontent.com/aslowwriter/dotfiles/main/setup/setup.sh -o /home/sam/setup.sh
 chmod +x /home/sam/setup.sh
-chown sam /home/sam/setup.sh
+chown sam:sam /home/sam/setup.sh
 
 bootctl list
